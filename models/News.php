187da91ccebcd27d1b457dad6b70ca70965c14d0
seredops <48289291+seredops@users.noreply.github.com>
2019-07-20 09:18:07 +0300

    Add files via upload

---
+++ models/News.php
@@ -0,0 +1,12 @@
+<?php
+class News
+{
+    public static function getNewsItemById($id)
+    {
+        //запрос к бд
+    }
+    public  static function getNewsList()
+    {
+        // запрос к бд
+    }
+}
